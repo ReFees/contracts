1af5e35e5dd150ed56238f7c56e691bdf2e7de56
Lucas Giordano <giordano3102lucas@gmail.com>
2021-11-21 00:47:18 +0100

    reorganisation of directories + use RefeesToken as currency (simplification)

--- contracts/RefeesFactory.sol
+++ contracts/RefeesFactory.sol
@@ -1,14 +1,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.2;
 
-import './RefeesClient.sol';
-import './RefeesProvider.sol';
-import './RefeesSubscription.sol';
-import './RefeesEvents.sol';
+import "./tokens/RefeesToken.sol";
+import "./tokens/RefeesClient.sol";
+import "./tokens/RefeesProvider.sol";
+import "./helpers/RefeesEvents.sol";
+import "./RefeesSubscription.sol";
 
 contract RefeesFactory is RefeesEvents {
-    
-    // NFT Tokens
+    // system tokens
+    RefeesToken immutable refT;
     RefeesClient immutable refC;
     RefeesProvider immutable refP;
     // Array of subscriptions
@@ -20,10 +21,22 @@ contract RefeesFactory is RefeesEvents {
         refP = new RefeesProvider();
     }
 
-    function createSubscription(uint256 _maturity, uint256 _frequency, uint256 _paymentAmount,uint256 _initialReserve) external {
-        RefeesSubscription refS = new RefeesSubscription(_maturity, _frequency, _paymentAmount, _initialReserve, refC, refP);
+    function createSubscription(
+        uint256 _maturity,
+        uint256 _frequency,
+        uint256 _paymentAmount,
+        uint256 _initialReserve
+    ) external {
+        RefeesSubscription refS = new RefeesSubscription(
+            _maturity,
+            _frequency,
+            _paymentAmount,
+            _initialReserve,
+            refT,
+            refC,
+            refP
+        );
         subscriptions.push(refS);
         emit SubscriptionCreated(block.timestamp, address(refS));
     }
-    
 }
--- contracts/RefeesSubscription.sol
+++ contracts/RefeesSubscription.sol
@@ -1,19 +1,23 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.2;
 
-import './RefeesClient.sol';
-import './RefeesProvider.sol';
-import './RefeesPool.sol';
+import './tokens/RefeesToken.sol';
+import './tokens/RefeesClient.sol';
+import './tokens/RefeesProvider.sol';
+import './helpers/RefeesPool.sol';
 
 contract RefeesSubscription {
     // contract variables
     uint256 immutable public maturity;
     uint256 immutable public frequency;
     uint256 immutable public paymentAmount;
+    uint256 immutable public totalExpectedPaymentAmount;
     uint256 immutable public initialReserve;
     // NFT Tokens
     RefeesClient immutable refC;
     RefeesProvider immutable refP;
+    // native Token 
+    RefeesToken immutable refT;
     // tokenId associated the the NFT
     uint256 public immutable clientId;
     uint256 public immutable providerId;
@@ -21,15 +25,18 @@ contract RefeesSubscription {
     bool private started;
     RefeesPool pool;
 
-    constructor(uint256 _maturity, uint256 _frequency, uint256 _paymentAmount,uint256 _initialReserve, RefeesClient _refC, RefeesProvider _refP) {
+    constructor(uint256 _maturity, uint256 _frequency, uint256 _paymentAmount,uint256 _initialReserve, RefeesToken _refT, RefeesClient _refC, RefeesProvider _refP) {
         // contract variables
         maturity  = _maturity;
         frequency = _frequency;
         paymentAmount  = _paymentAmount;
         initialReserve = _initialReserve;
-        // mint provider and client NFTs
+        totalExpectedPaymentAmount = _frequency * _paymentAmount;
+        // system tokens
+        refT = _refT;
         refC = _refC;
         refP = _refP;
+        // mint provider and client NFTs
         clientId   = refC.safeMint(msg.sender);
         providerId = refP.safeMint(msg.sender);
         // state variables
@@ -37,20 +44,19 @@ contract RefeesSubscription {
         pool    = new RefeesPool(_initialReserve);
     }
 
-    function subscribe() public returns (bool) {
-        
-    }
 
     function startSubscription() public returns (bool) {
+        require(refC.ownerOf(clientId) != refP.ownerOf(providerId));
+        require(refT.allowance(refC.ownerOf(clientId), address(this)) >= totalExpectedPaymentAmount);
         started = true;
         return true;
     }
 
-    function refund(uint256 gasAmount) public {
+    function refund(uint256 gasAmount) public returns (bool) {
         // get client address
         address addrClient = refC.ownerOf(clientId);
-
-
+        pool.withdraw(gasAmount);
+        return refT.transfer(addrClient, gasAmount);
     }
 
 
